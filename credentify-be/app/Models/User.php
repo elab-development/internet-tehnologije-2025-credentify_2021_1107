65fb821b565047da4e796915392c3c71d10cd988
Nikola <nikolanedeljkovic226@gmail.com>
2026-02-01 22:28:04 +0100

    Izmena user modela da odgovara pmovu

--- credentify-be/app/Models/User.php
+++ credentify-be/app/Models/User.php
@@ -2,47 +2,54 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    public const ROLE_USER  = 'user';
+    public const ROLE_ADMIN = 'admin';
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'profile_info',
+        'role',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
      */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Always hash password when setting it.
      */
-    protected function casts(): array
+    public function setPasswordAttribute($value): void
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        if (!empty($value)) {
+            $this->attributes['password'] = bcrypt($value);
+        }
+    }
+
+    /**
+     * 1:N veza. Jedan User ima više UserCredential zapisa.
+     */
+    public function userCredentials(): HasMany
+    {
+        return $this->hasMany(UserCredential::class);
     }
 }
