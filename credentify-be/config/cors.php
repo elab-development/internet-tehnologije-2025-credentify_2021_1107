7da82eba3bbd057bf531081a60227fc9b590ec22
Nikola <nikolanedeljkovic226@gmail.com>
2026-02-16 19:38:39 +0100

    Izmenaa greskice

--- credentify-be/config/cors.php
+++ credentify-be/config/cors.php
@@ -2,28 +2,22 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://127.0.0.1:3000',
-    ],
+        'http://127.0.0.1:3000' ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [
-        'Content-Type',
-        'Authorization',
-        'X-Requested-With',
-        'Accept',
-        'Origin',
-    ],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 ];
